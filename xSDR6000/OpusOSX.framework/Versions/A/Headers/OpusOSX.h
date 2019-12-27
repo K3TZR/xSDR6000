ba8b32445cb8ae238ea44c3078c5895e5842d45c
Douglas Adams <douglas.adams@me.com>
2019-12-27 14:05:47 -0500

    Initial commit

---
+++ xSDR6000/OpusOSX.framework/Versions/A/Headers/OpusOSX.h
@@ -0,0 +1,26 @@
+//
+//  OpusOSX.h
+//  OpusOSX
+//
+//  Created by Marc Delling on 01.08.15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OpusOSX.
+FOUNDATION_EXPORT double OpusOSXVersionNumber;
+
+//! Project version string for OpusOSX.
+FOUNDATION_EXPORT const unsigned char OpusOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpusOSX/PublicHeader.h>
+
+//#import <OpusOSX/opusfile.h>
+//#import <OpusOSX/ogg.h>
+//#import <OpusOSX/os_types.h>
+#import <OpusOSX/opus.h>
+#import <OpusOSX/opus_multistream.h>
+#import <OpusOSX/opus_custom.h>
+#import <OpusOSX/opus_types.h>
+#import <OpusOSX/opus_defines.h>
