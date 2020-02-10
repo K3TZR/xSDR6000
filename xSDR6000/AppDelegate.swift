a4b45835ac5770e6edf153c1e436e0d874348c26
Douglas Adams <douglas.adams@me.com>
2020-02-09 23:37:55 -0500

    Changed the way Version is used

--- xSDR6000/AppDelegate.swift
+++ xSDR6000/AppDelegate.swift
@@ -19,7 +19,7 @@ final class AppDelegate                     : NSObject, NSApplicationDelegate , 
   
   // App parameters
   static let kName                          = "xSDR6000"
-  static let kVersion                       = Version("1.0.6" )
+  static let kVersion                       = Version()
   
   // Log parameters
   static let kLoggerName                    = AppDelegate.kName
